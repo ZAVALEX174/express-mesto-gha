eeca1b5178e9dfee0cf965018095c6fe435b5cf5
Alexandr Zuev <zuevalex@yandex.ru>
2023-06-12 02:30:41 +0500

    3000/404

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 
-const { NotFoundError } = require('../errors/NotFoundError');
+const ErrorNoteFound = 404;
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 router.use('*', (req, res) => {
-  res.status(NotFoundError).send({ message: 'Запрашиваемый ресурс не найден' });
+  res.status(ErrorNoteFound).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 module.exports = router;
