4c797c520b5ed684a98c209ebc3ef3e636bc5172
Alexandr Zuev <zuevalex@yandex.ru>
2023-06-21 03:02:41 +0500

    что то там вроде как поправил2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,35 +1,5 @@
 const jwt = require('jsonwebtoken');
-// const { UnauthorizedError } = require('../errors/UnauthorizedError');
-
-// function auth(req, res, next) {
-//   try {
-//     const { authorization } = req.headers;
-
-//     if (!authorization || !authorization.startsWith('Bearer ')) {
-//       throw new UnauthorizedError(
-//         'Для выполн!ения действия необходима авторизация',
-//       );
-//     }
-
-//     const token = authorization.replace('Bearer ', '');
-//     let payload;
-
-//     try {
-//       payload = jwt.verify(token, 'secretkey');
-//     } catch (err) {
-//       throw new UnauthorizedError(
-//         'Для выполнения действия необходима авторизация',
-//       );
-//     }
-
-//     req.user = payload;
-//     next();
-//   } catch (err) {
-//     next(err);
-//   }
-// }
-
-// module.exports = { auth };
+const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -38,8 +8,8 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'secretkey');
   } catch (err) {
-    // throw new UnauthorizedError('Необходима авторизация');
-    next(err);
+    throw new UnauthorizedError('Необходима авторизация');
+    // next(err);
   }
 
   req.user = payload;
