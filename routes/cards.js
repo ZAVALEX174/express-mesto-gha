de104863f6a495a26b1a06afedd42fa96b01b9c8
Alexandr Zuev <zuevalex@yandex.ru>
2023-06-21 04:14:07 +0500

    после первого ревью2

--- routes/cards.js
+++ routes/cards.js
@@ -26,7 +26,7 @@ router.post(
       name: Joi.string().required().min(2).max(30),
       link: Joi.string()
         .required()
-        .regex(/https?:\/\/(www)?[0-9a-z\-._~:/?#[\]@!$&'()*+,;=]+#?$/i),
+        .regex(/^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
     }),
   }),
   createCard,
